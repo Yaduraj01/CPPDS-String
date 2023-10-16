2c33e2284a5c07a115ca91608596abb8ba4812be
Yaduraj01 <110488113+Yaduraj01@users.noreply.github.com>
2023-10-16 21:24:54 +0530

    Add files via upload

---
+++ lab_string.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+/*To take string input*/
+/*main()
+{
+    char n[10];
+    cout << "enter a name: ";
+    cin >> n;
+    cout << n;
+}*/
+
+/*To find the length*/
+/*main()
+{
+    int i;
+    char n[10];
+    cout << "enter a name: ";
+    cin >> n;
+    for(i=0;n[i]!='\0';i++)
+    {
+    }
+    cout << i;
+}*/
+
+/*To print reverse the string*/
+main()
+{
+    char n[10];
+    int i;
+    cout << "enter a name: ";
+    cin >> n;
+    for(i=0;n[i]!='\0';i++)
+    {
+    }
+    for(int j=i-1;i>0;i--)
+    {
+        cout << n[i];
+    }
+}
+
